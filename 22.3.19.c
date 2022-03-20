0c90c84c783a9c27b6548b24d5ce3333faca0957
XuYj1020 <x123j12@163.com>
2022-03-20 17:55:28 +0800

    Create 22.3.19.c

---
+++ 22.3.19.c
@@ -0,0 +1,47 @@
+//第一天22.3.19
+// 输出hallo world
+//#include <stdio.h> //包含一个叫 stdio.h 的文件
+//char main()//主函数-程序入口-不可或缺且只有一个
+//{
+//	printf("hallo world\n"); // \n 换行
+//	return 0;
+//}
+
+/* #include<> 调用库函数 */
+/* main函数的结构，main前面的int表示main函数调用后返回一个整型值
+* int main()
+* {
+*  return 0;//返回0
+   }
+*/
+
+//第二天22.3.20
+/*数据类型
+char        字符数据类型
+short       短整型
+int         整形
+long        长整型
+long long   更长的整型
+float       单精度浮点数
+double      双精度浮点数
+*/
+
+// #include<stdio.h>
+// int main() {
+// 	//char ch = 'a';
+// 	//printf("%c\n",ch); // %c -- 打印字符格式的数据
+// 	//int age = 20;
+// 	//printf("%d\n",age);
+// 	//float fl = 20.66;
+// 	//printf("%f\n", fl);
+// 	// 二进制转十进制  011  1*10^0+1*10^2+0*10^3
+//     //查看各种字符类型的大小     
+// 	printf("%d\n", sizeof(char));        // 1byte=8bit  
+// 	printf("%d\n", sizeof(short));       // 2byte=16bit 最大数字为2^16-1
+// 	printf("%d\n", sizeof(int));         // 4
+// 	printf("%d\n", sizeof(long));        // 4
+// 	printf("%d\n", sizeof(long long));   // 8
+// 	printf("%d\n", sizeof(float));       // 4
+// 	printf("%d\n", sizeof(double));      // 8
+//  	return 0;
+// }
